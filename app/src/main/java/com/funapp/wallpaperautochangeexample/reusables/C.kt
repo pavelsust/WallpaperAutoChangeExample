444a85098d04ef7ccf461a784bd7623f5585b68f
paveltech <pavelcep.sust11@gmail.com>
2019-10-23 01:28:09 +0600

    Initial commit

---
+++ app/src/main/java/com/funapp/wallpaperautochangeexample/reusables/C.kt
@@ -0,0 +1,16 @@
+package com.funapp.wallpaperautochangeexample.reusables
+
+
+
+const val AUTO_WALLPAPER = "autoWallpaper"
+const val ANY = "any"
+const val CRASHLYTICS = "crashlytics"
+const val ANALYTICS = "analytics"
+const val WALL_CHANGE = "wallChange"
+const val CACHED = "cached"
+const val CACHE_NUMBER = "cacheNumber"
+const val DELETE_CACHE = "deleteCache"
+const val SHOW_TOAST = "showToast"
+const val RATE = "rate"
+const val DIALOG = "dialog"
+const val IMAGE = "image"
