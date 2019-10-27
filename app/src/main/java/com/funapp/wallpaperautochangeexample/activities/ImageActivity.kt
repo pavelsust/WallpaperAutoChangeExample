14cdedc01d69f4e67a38c46e6085bf5a931aa86a
paveltech <pavelcep.sust11@gmail.com>
2019-10-28 01:40:32 +0600

    image activity addd

--- app/src/main/java/com/funapp/wallpaperautochangeexample/activities/ImageActivity.kt
+++ app/src/main/java/com/funapp/wallpaperautochangeexample/activities/ImageActivity.kt
@@ -1,13 +1,35 @@
 package com.funapp.wallpaperautochangeexample.activities
 
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import com.funapp.wallpaperautochangeexample.R
+import com.funapp.wallpaperautochangeexample.reusables.IMAGE
+import kotlinx.android.synthetic.main.activity_image.*
 
-class ImageActivity : AppCompatActivity() {
+class ImageActivity : AppCompatActivity()  , View.OnClickListener{
+
+
+    private lateinit var image: Bitmap
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_image)
+
+        image = BitmapFactory.decodeFile(intent.getStringExtra(IMAGE))
+        bgWallpaper.setImageBitmap(image)
+
+        download.setOnClickListener(this)
+        setWallpaper.setOnClickListener(this)
+        back.setOnClickListener(this)
+
+
+    }
+
+
+    override fun onClick(v: View?) {
+
     }
 }
