b1c566f5a7cc1538d46659a36eb2710edae6dab3
paveltech <pavelcep.sust11@gmail.com>
2019-10-28 00:24:38 +0600

    image added

--- app/src/main/java/com/funapp/wallpaperautochangeexample/adapter/HolderImage.kt
+++ app/src/main/java/com/funapp/wallpaperautochangeexample/adapter/HolderImage.kt
@@ -2,7 +2,12 @@ package com.funapp.wallpaperautochangeexample.adapter
 
 import android.graphics.BitmapFactory
 import android.view.View
+import android.widget.FrameLayout
 import androidx.recyclerview.widget.RecyclerView
+import com.funapp.wallpaperautochangeexample.activities.ImageActivity
+import com.funapp.wallpaperautochangeexample.functions.F
+import com.funapp.wallpaperautochangeexample.functions.openNewActivity
+import com.funapp.wallpaperautochangeexample.reusables.IMAGE
 import kotlinx.android.synthetic.main.inflator_image.view.*
 import java.io.File
 
@@ -16,7 +21,28 @@ class HolderImage(view: View): RecyclerView.ViewHolder(view){
         val bitmap = BitmapFactory.decodeFile(file.path)
 
         bitmap.let {
-            
+            // calculating dynamic height
+
+            val point = F.displayDimensions(context)
+            val width = point.x/2
+            val height = F.getDynamicHeight(context , point.x/2 ,point.y,bitmap.width,bitmap.height)
+
+            //change layout parm based on image
+
+            layout.layoutParams = FrameLayout.LayoutParams(width-F.dpToPx(4, context) , height)
+            image.layoutParams = FrameLayout.LayoutParams(width-F.dpToPx(8 , context) , height)
+
+            // set bitmap
+            image.setImageBitmap(it)
+
+
+            // handel image click
+
+            image.setOnClickListener {
+                context.openNewActivity(ImageActivity::class.java){
+                    putString(IMAGE , file.path)
+                }
+            }
         }
     }
 }
