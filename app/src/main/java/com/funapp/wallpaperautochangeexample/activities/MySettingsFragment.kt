035c62fa4818ba6dc086005f2f5afb4ffa1829cd
paveltech <pavelcep.sust11@gmail.com>
2019-11-05 02:11:19 +0600

    preferance added

--- app/src/main/java/com/funapp/wallpaperautochangeexample/activities/MySettingsFragment.kt
+++ app/src/main/java/com/funapp/wallpaperautochangeexample/activities/MySettingsFragment.kt
@@ -1,4 +1,38 @@
 package com.funapp.wallpaperautochangeexample.activities
 
-class MySettingsFragment {
+import android.os.Bundle
+import androidx.preference.*
+import com.funapp.wallpaperautochangeexample.R
+
+
+class MySettingsFragment : PreferenceFragmentCompat() , Preference.OnPreferenceChangeListener  {
+
+
+    private lateinit var wallpaperStatus: SwitchPreference
+    private lateinit var wallpaperInterval: ListPreference
+    private lateinit var wallpaperWifi: SwitchPreference
+    private lateinit var crashlytics: SwitchPreference
+    private lateinit var analytics: SwitchPreference
+    private lateinit var wallpaperChange: SwitchPreference
+    private lateinit var search : EditTextPreference
+    private lateinit var cacheNumber: ListPreference
+    private lateinit var cacheClear: Preference
+
+
+
+    override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
+        setPreferencesFromResource(R.xml.preferences, rootKey)
+
+        wallpaperStatus = findPreference("wallStatus")!!
+        wallpaperInterval = findPreference("wallInterval")!!
+        wallpaperWifi = findPreference()
+    }
+
+
+
+    override fun onPreferenceChange(preference: Preference?, newValue: Any?): Boolean {
+        return true
+    }
+
+
 }
