6df123ef0f6aa6df7d02eed6e541eabecd6d6b7d
paveltech <pavelcep.sust11@gmail.com>
2019-11-06 01:08:19 +0600

    wallpaper activity design

---
+++ app/src/main/java/com/funapp/wallpaperautochangeexample/activities/WallpaperActivity.kt
@@ -0,0 +1,13 @@
+package com.funapp.wallpaperautochangeexample.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.funapp.wallpaperautochangeexample.R
+
+class WallpaperActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_wallpaper)
+    }
+}
